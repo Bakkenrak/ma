7dd79d973a664ae74412cca7e34c160790f864b5
Bakkenrak <christian.rest@hotmail.de>
2015-09-01 13:32:01 +0200

    Added sortability and extended filtering to platforms and suggestions overviews

--- discover2share-Web/WebContent/resources/js/controller/platformsCtrl.js
+++ discover2share-Web/WebContent/resources/js/controller/platformsCtrl.js
@@ -3,20 +3,47 @@
 
 	var d2sApp = angular.module('d2sApp');
 
-	d2sApp.controller('platformsCtrl', function ($scope, $location, toaster, platforms) {
+	d2sApp.controller('platformsCtrl', function ($scope, $location, $filter, toaster, platforms) {
 		if (platforms.status === 401) {
 			toaster.pop("error", "Unauthorized!", "You need to be logged in to access the information on that page.");
 			$location.path("login/");
 		}
 		
+		platforms.data.forEach(function (platform) {		
+			platform.resourceTypesJoined = $filter('orderBy')(platform.resourceTypes, 'label')
+												.map(function (item) {
+													return item.label;
+												}).join(", ");
+		});
+		
 		$scope.pagination = {
 			itemsPerPage : 25
 		};
 		$scope.searchTerm = "";
+		
+		$scope.orderBy = {
+			attribute: 'label',
+			reverse: false
+		};
+		
+		$scope.changeOrderBy = function (attribute) {
+			if (attribute === $scope.orderBy.attribute) {
+				$scope.orderBy.reverse = !$scope.orderBy.reverse;
+			} else {
+				$scope.orderBy.attribute = attribute;
+				$scope.orderBy.reverse = false;
+			}
+			$scope.filteredPlatforms = $filter('orderBy')($scope.platforms, $scope.orderBy.attribute, $scope.orderBy.reverse);
+			$scope.doPagination();
+		};
 
 		// triggered when the current page of pagination, the
 		// items per page or the filtered platform array change
 		$scope.$watch("pagination.currentPage + pagination.itemsPerPage + filteredPlatforms", function () {
+			$scope.doPagination();
+		});
+		
+		$scope.doPagination = function () {
 			$scope.pagination.begin = (($scope.pagination.currentPage - 1) * $scope.pagination.itemsPerPage);
 			$scope.pagination.end = $scope.pagination.begin	+ $scope.pagination.itemsPerPage;
 			// adjust end if it exceeds the actual numer of platforms
@@ -25,21 +52,23 @@
 			}
 			// slice the platforms for display according to currently selected page
 			$scope.paginatedPlatforms = $scope.filteredPlatforms.slice($scope.pagination.begin, $scope.pagination.end);
-		});
+		};
 
 		// triggered when the search term input changes
 		$scope.$watch("searchTerm",	function () {
 			$scope.filteredPlatforms = []; // empty filtered list
 			angular.forEach($scope.platforms, function (platform) {
-				// add those whose label contains the search term
-				if (platform.label !== null	&& ~platform.label.toLowerCase().indexOf($scope.searchTerm)) {
+				var searchTermLower = $scope.searchTerm.toLowerCase();
+				// add those whose label or resource types contain the search term
+				if ((platform.label !== null	&& ~platform.label.toLowerCase().indexOf(searchTermLower)) || 
+						(platform.resourceTypesJoined !== null && ~platform.resourceTypesJoined.toLowerCase().indexOf(searchTermLower))) {
 					$scope.filteredPlatforms.push(platform);
 				}
 			});
 			$scope.pagination.currentPage = 1; // set pagination to first page
 		});
 
-		$scope.filteredPlatforms = $scope.platforms = platforms.data;
+		$scope.filteredPlatforms = $scope.platforms = $filter('orderBy')(platforms.data, $scope.orderBy.attribute, $scope.orderBy.reverse);
 	});
 	
 })();
